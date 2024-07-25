5700070817d5c26050ba4372c4f9a15dbc81bdb9
Dhineshvaran-S <dhineshvaran2000@gmail.com>
2024-07-25 11:46:56 +0530

    Add files via upload

---
+++ Java Script Task/function.js
@@ -0,0 +1,4 @@
+function myfunction(fname, mname, lname) {
+    return "FirstName:".concat(fname, " MiddelName:").concat(mname, " LastName:").concat(lname);
+}
+console.log(myfunction("kuruvi", "kuru", "kuruviii"));
---
+++ Java Script Task/function.ts
@@ -0,0 +1,10 @@
+
+
+function myfunction(fname:string,mname:string,lname:string) {
+
+    return `FirstName:${fname} MiddelName:${mname} LastName:${lname }`
+    
+
+}
+console.log(myfunction("kuruvi","kuru","kuruviii"));
+
